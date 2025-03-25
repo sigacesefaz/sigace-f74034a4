e0a5290059b9499e37d1b81fc18e48f244ff5df7
sigacesefaz <sigacesefaz@hotmail.com>
2025-03-25 10:59:27 -0300

    Pasta Desnecessaria Apagada
    Pasta Desnecessaria Apagada

--- sigace-f74034a4-main/src/components/ui/use-toast.ts
+++
@@ -1,5 +0,0 @@
-
-// Re-export from the hooks file
-import { useToast, toast } from "@/hooks/use-toast";
-
-export { useToast, toast };
--- sigace-f74034a4-main/src/lib/tjto.d.ts
+++
@@ -1,5 +0,0 @@
-import { ProcessHit } from '@/types/process';
-
-declare module '@/lib/tjto' {
-  export function checkProcessStatus(processNumber: string): Promise<ProcessHit | null>;
-} 
